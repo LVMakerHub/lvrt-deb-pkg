f4ac0f75e97cd0555a635d56c0966d89fd658112
Ken Sharp <ken.sharp@ni.com>
2015-11-03 14:25:28 -0600

    Fix power off issue with labview service
    If the target is not shut down properly (power is abruptly turned off),
    the labview run-time daemon would not start properly on a subsequent boot.
    This happened because a schroot session file, that is usually deleted when a
    session is ended properly, was left behind.
    
    Change the labview.service to call a script which checks for the existence
    of the session file and deletes it before starting the labview daemon.

---
+++ src/schroot-lv-start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+LV_SESSION_FILE=/var/lib/schroot/session/lv
+
+# check to make sure that the last schroot session shut down correctly
+if [ -e $LV_SESSION_FILE ]; then
+	rm $LV_SESSION_FILE
+fi
+
+# start the session
+/usr/bin/schroot --begin-session --session-name=lv -c labview
